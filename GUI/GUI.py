95d8fbbdca1637d499652682ba0ae503920d78e9
Geileren <jakob.geil@gmail.com>
2021-05-06 14:12:14 +0200

    Ændrede stavefejl

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -3,49 +3,49 @@ from tkinter import *
 #from PIL import Image, ImageTk
 import sys
 class Felt():
-    def __init__(self, root, bræt, billede, x, y, brækker):
-        self.brækker = brækker
+    def __init__(self, root, bræt, billede, x, y, brikker):
+        self.brikker = brikker
         self.bræt = bræt
-        self.bræk = billede
+        self.brik = billede
         self.x = x
         self.y = y
-        self.button = Button(root, image=self.bræk, command=self.tryk)
+        self.button = Button(root, image=self.brik, command=self.tryk)
 
 
     def check_farve(self):
         if (self.x + self.y) % 2 == 0:
-            if self.brækker.index(self.bræk) % 2 != 0:
-                self.skift(self.brækker[self.brækker.index(self.bræk) - 1])
+            if self.brikker.index(self.brik) % 2 != 0:
+                self.skift(self.brikker[self.brikker.index(self.brik) - 1])
 
             else:
                 pass
         else:
-            if self.brækker.index(self.bræk) % 2 != 1:
-                self.skift(self.brækker[self.brækker.index(self.bræk) + 1])
+            if self.brikker.index(self.brik) % 2 != 1:
+                self.skift(self.brikker[self.brikker.index(self.brik) + 1])
             else:
                 pass
 
 
     def skift(self, billede):
-        self.bræk = billede
-        self.button.config(image=self.bræk)
+        self.brik = billede
+        self.button.config(image=self.brik)
 
     def grid(self, x, y):
         self.button.grid(column=x, row=y)
 
     def tryk(self):
-        if self.bræt.flyt_bræk == False:
-            self.bræt.flyt_bræk = True
-            self.bræt.nuværende_bræk = self.bræk
-            self.bræt.flyt_bræk_pos = self.x, self.y
+        if self.bræt.flyt_brik == False:
+            self.bræt.flyt_brik = True
+            self.bræt.nuværende_brik = self.brik
+            self.bræt.flyt_brik_pos = self.x, self.y
             self.button.config(relief="sunken", bg="yellow")
-        elif self.bræt.flyt_bræk == True:
-            if self.x == self.bræt.flyt_bræk_pos[0] and self.y == self.bræt.flyt_bræk_pos[1]:
-                self.bræt.flyt_bræk = False
+        elif self.bræt.flyt_brik == True:
+            if self.x == self.bræt.flyt_brik_pos[0] and self.y == self.bræt.flyt_brik_pos[1]:
+                self.bræt.flyt_brik = False
                 self.button.config(relief="raised", bg="white")
             else:
                 #begynd flyt, skal snakke med logik
-                self.skift(self.bræt.nuværende_bræk)
+                self.skift(self.bræt.nuværende_brik)
                 self.check_farve()
                 pass
 
@@ -61,9 +61,9 @@ class Bræt:
         self.tildel_billeder(self.brækker)
         self.gen_start_lister()
         self.table_fields = []
-        self.flyt_bræk = False
-        self.nuværende_bræk = ""
-        self.flyt_bræk_pos = ""
+        self.flyt_brik = False
+        self.nuværende_brik = ""
+        self.flyt_brik_pos = ""
 
 
 
@@ -76,45 +76,45 @@ class Bræt:
 
         self.hvid_start_bønder = self.hvid_bonde_mørk, self.hvid_bonde_lys
 
-    def tildel_billeder(self, brækker):
-        self.sort_bonde_mørk = brækker[0]
-        self.sort_bonde_lys = brækker[1]
+    def tildel_billeder(self, brikker):
+        self.sort_bonde_mørk = brikker[0]
+        self.sort_bonde_lys = brikker[1]
 
-        self.sort_løber_mørk = brækker[2]
-        self.sort_løber_lys = brækker[3]
+        self.sort_løber_mørk = brikker[2]
+        self.sort_løber_lys = brikker[3]
 
-        self.sort_springer_mørk = brækker[4]
-        self.sort_springer_lys = brækker[5]
+        self.sort_springer_mørk = brikker[4]
+        self.sort_springer_lys = brikker[5]
 
-        self.sort_tårn_mørk = brækker[6]
-        self.sort_tårn_lys = brækker[7]
+        self.sort_tårn_mørk = brikker[6]
+        self.sort_tårn_lys = brikker[7]
 
-        self.sort_konge_mørk = brækker[8]
-        self.sort_konge_lys = brækker[9]
+        self.sort_konge_mørk = brikker[8]
+        self.sort_konge_lys = brikker[9]
 
-        self.sort_dronning_mørk = brækker[10]
-        self.sort_dronning_lys = brækker[11]
+        self.sort_dronning_mørk = brikker[10]
+        self.sort_dronning_lys = brikker[11]
 
-        self.tom_mørk = brækker[12]
-        self.tom_lys = brækker[13]
+        self.tom_mørk = brikker[12]
+        self.tom_lys = brikker[13]
 
-        self.hvid_bonde_mørk = brækker[14]
-        self.hvid_bonde_lys = brækker[15]
+        self.hvid_bonde_mørk = brikker[14]
+        self.hvid_bonde_lys = brikker[15]
 
-        self.hvid_løber_mørk = brækker[16]
-        self.hvid_løber_lys = brækker[17]
+        self.hvid_løber_mørk = brikker[16]
+        self.hvid_løber_lys = brikker[17]
 
-        self.hvid_springer_mørk = brækker[18]
-        self.hvid_springer_lys = brækker[19]
+        self.hvid_springer_mørk = brikker[18]
+        self.hvid_springer_lys = brikker[19]
 
-        self.hvid_tårn_mørk = brækker[20]
-        self.hvid_tårn_lys = brækker[21]
+        self.hvid_tårn_mørk = brikker[20]
+        self.hvid_tårn_lys = brikker[21]
 
-        self.hvid_konge_mørk = brækker[22]
-        self.hvid_konge_lys = brækker[23]
+        self.hvid_konge_mørk = brikker[22]
+        self.hvid_konge_lys = brikker[23]
 
-        self.hvid_dronning_mørk = brækker[24]
-        self.hvid_dronning_lys = brækker[25]
+        self.hvid_dronning_mørk = brikker[24]
+        self.hvid_dronning_lys = brikker[25]
 
     def lav(self):
         for y in range(1,9):
@@ -146,87 +146,87 @@ class Bræt:
 
 
 def gen_picts(path, size): #importere og subsampler alle billederne
-    brækker = []
+    brikker = []
     sort_bonde_mørk = PhotoImage(file=rf"{path}\Assets\sort_bonde_mørk.png")
-    brækker.append(sort_bonde_mørk.subsample(size, size))
+    brikker.append(sort_bonde_mørk.subsample(size, size))
 
     sort_bonde_lys = PhotoImage(file=rf"{path}\Assets\sort_bonde_lys.png")
-    brækker.append(sort_bonde_lys.subsample(size, size))
+    brikker.append(sort_bonde_lys.subsample(size, size))
 
     sort_løber_mørk = PhotoImage(file=rf"{path}\Assets\sort_løber_mørk.png")
-    brækker.append(sort_løber_mørk.subsample(size, size))
+    brikker.append(sort_løber_mørk.subsample(size, size))
 
     sort_løber_lys = PhotoImage(file=rf"{path}\Assets\sort_løber_lys.png")
-    brækker.append(sort_løber_lys.subsample(size, size))
+    brikker.append(sort_løber_lys.subsample(size, size))
 
     sort_springer_mørk = PhotoImage(file=rf"{path}\Assets\sort_springer_mørk.png")
-    brækker.append(sort_springer_mørk.subsample(size, size))
+    brikker.append(sort_springer_mørk.subsample(size, size))
 
     sort_springer_lys = PhotoImage(file=rf"{path}\Assets\sort_springer_lys.png")
-    brækker.append(sort_springer_lys.subsample(size, size))
+    brikker.append(sort_springer_lys.subsample(size, size))
 
     sort_tårn_mørk = PhotoImage(file=rf"{path}\Assets\sort_tårn_mørk.png")
-    brækker.append(sort_tårn_mørk.subsample(size, size))
+    brikker.append(sort_tårn_mørk.subsample(size, size))
 
     sort_tårn_lys = PhotoImage(file=rf"{path}\Assets\sort_tårn_lys.png")
-    brækker.append(sort_tårn_lys.subsample(size, size))
+    brikker.append(sort_tårn_lys.subsample(size, size))
 
     sort_konge_mørk = PhotoImage(file=rf"{path}\Assets\sort_konge_mørk.png")
-    brækker.append(sort_konge_mørk.subsample(size, size))
+    brikker.append(sort_konge_mørk.subsample(size, size))
 
     sort_konge_lys = PhotoImage(file=rf"{path}\Assets\sort_konge_lys.png")
-    brækker.append(sort_konge_lys.subsample(size, size))
+    brikker.append(sort_konge_lys.subsample(size, size))
 
     sort_dronning_mørk = PhotoImage(file=rf"{path}\Assets\sort_dronning_mørk.png")
-    brækker.append(sort_dronning_mørk.subsample(size, size))
+    brikker.append(sort_dronning_mørk.subsample(size, size))
 
     sort_dronning_lys = PhotoImage(file=rf"{path}\Assets\sort_dronning_lys.png")
-    brækker.append(sort_dronning_lys.subsample(size, size))
+    brikker.append(sort_dronning_lys.subsample(size, size))
 
     tom_mørk = PhotoImage(file=rf"{path}\Assets\tom_mørk.png")
-    brækker.append(tom_mørk.subsample(size, size))
+    brikker.append(tom_mørk.subsample(size, size))
 
     tom_lys = PhotoImage(file=rf"{path}\Assets\tom_lys.png")
-    brækker.append(tom_lys.subsample(size, size))
+    brikker.append(tom_lys.subsample(size, size))
 
     hvid_bonde_mørk = PhotoImage(file=rf"{path}\Assets\hvid_bonde_mørk.png")
-    brækker.append(hvid_bonde_mørk.subsample(size, size))
+    brikker.append(hvid_bonde_mørk.subsample(size, size))
 
     hvid_bonde_lys = PhotoImage(file=rf"{path}\Assets\hvid_bonde_lys.png")
-    brækker.append(hvid_bonde_lys.subsample(size, size))
+    brikker.append(hvid_bonde_lys.subsample(size, size))
 
     hvid_løber_mørk = PhotoImage(file=rf"{path}\Assets\hvid_løber_mørk.png")
-    brækker.append(hvid_løber_mørk.subsample(size, size))
+    brikker.append(hvid_løber_mørk.subsample(size, size))
 
     hvid_løber_lys = PhotoImage(file=rf"{path}\Assets\hvid_løber_lys.png")
-    brækker.append(hvid_løber_lys.subsample(size, size))
+    brikker.append(hvid_løber_lys.subsample(size, size))
 
     hvid_springer_mørk = PhotoImage(file=rf"{path}\Assets\hvid_springer_mørk.png")
-    brækker.append(hvid_springer_mørk.subsample(size, size))
+    brikker.append(hvid_springer_mørk.subsample(size, size))
 
     hvid_springer_lys = PhotoImage(file=rf"{path}\Assets\hvid_springer_lys.png")
-    brækker.append(hvid_springer_lys.subsample(size, size))
+    brikker.append(hvid_springer_lys.subsample(size, size))
 
     hvid_tårn_mørk = PhotoImage(file=rf"{path}\Assets\hvid_tårn_mørk.png")
-    brækker.append(hvid_tårn_mørk.subsample(size, size))
+    brikker.append(hvid_tårn_mørk.subsample(size, size))
 
     hvid_tårn_lys = PhotoImage(file=rf"{path}\Assets\hvid_tårn_lys.png")
-    brækker.append(hvid_tårn_lys.subsample(size, size))
+    brikker.append(hvid_tårn_lys.subsample(size, size))
 
     hvid_konge_mørk = PhotoImage(file=rf"{path}\Assets\hvid_konge_mørk.png")
-    brækker.append(hvid_konge_mørk.subsample(size, size))
+    brikker.append(hvid_konge_mørk.subsample(size, size))
 
     hvid_konge_lys = PhotoImage(file=rf"{path}\Assets\hvid_konge_lys.png")
-    brækker.append(hvid_konge_lys.subsample(size, size))
+    brikker.append(hvid_konge_lys.subsample(size, size))
 
     hvid_dronning_mørk = PhotoImage(file=rf"{path}\Assets\hvid_dronning_mørk.png")
-    brækker.append(hvid_dronning_mørk.subsample(size, size))
+    brikker.append(hvid_dronning_mørk.subsample(size, size))
 
     hvid_dronning_lys = PhotoImage(file=rf"{path}\Assets\hvid_dronning_lys.png")
-    brækker.append(hvid_dronning_lys.subsample(size, size))
+    brikker.append(hvid_dronning_lys.subsample(size, size))
 
 
-    return brækker
+    return brikker
 
 def main():
     root = Tk()
