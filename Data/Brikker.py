8d752b1e606cd59567e11aa4a05c7f103df4b2ba
Niclasapdk <74359616+Niclasapdk@users.noreply.github.com>
2021-05-23 15:32:34 +0200

    opdatering

--- Data/Brikker.py
+++ Data/Brikker.py
@@ -8,19 +8,6 @@ class Brik:
         self.kandræbes = kandræbes
 
 
-sb = Brik("s", "b")
-hb = Brik("h", "b")
-sk = Brik("s", "k")
-hk = Brik("h", "k")
-st = Brik("s", "t")
-ht = Brik("h", "t")
-ss = Brik("s", "s")
-hs = Brik("h", "s")
-sd = Brik("s", "d")
-hd = Brik("h", "d")
-sl = Brik("s", "l")
-hl = Brik("h", "l")
-
 def lav_braet(Braet):
     Braet[0] = [Brik("s", "t"), Brik("s", "s"), Brik("s", "l"), Brik("s", "d"), Brik("s", "k"), Brik("s", "l"), Brik("s", "s"), Brik("s", "t")]
     Braet[7] = [Brik("h", "t"), Brik("h", "s"), Brik("h", "l"), Brik("h", "d"), Brik("h", "k"), Brik("h", "l"), Brik("h", "s"), Brik("h", "t")]
